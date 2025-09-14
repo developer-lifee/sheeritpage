9074b2082c4ac335d867ead806cfe6b73b032cc6
estebandavid.avila.contractor <estebandavid.avila.contractor@bbva.com>
2025-09-14 08:43:50 -0500

    cambio en gpt

--- src/data/platforms.ts
+++ src/data/platforms.ts
@@ -1,210 +1,220 @@
 export const platforms = [
-      {
-        id: 1,
-        name: 'Netflix',
-        price: 13000,
-        image: 'https://th.bing.com/th/id/R.64d5f8b96325d2be917f9ab402e92b99?rik=1BawFvPFPBGxrg&pid=ImgRaw&r=0',
-        characteristics: [
-          "Cuenta estable y renovable",
-          "Alta definición 4K + HDR con su lista exclusiva",
-          "Entrega en tiempo real",
-        ],
-      },
-        {
-        id: 2,
-        name: 'Netflix Extra',
-        price: 17000,
-        image: 'https://th.bing.com/th/id/R.64d5f8b96325d2be917f9ab402e92b99?rik=1BawFvPFPBGxrg&pid=ImgRaw&r=0',
-        characteristics: [
-          "Cuenta completa con un perfil",
-          "Alta definición 4K + HDR con su lista exclusiva",
-          "correo personalizado, ideal para usos en diferentes locaciones",
-        ],
-      },
-      {
-        id: 5,
-        name: 'Disney+',
-        price: 13000,
-        image: 'https://mdcthereporter.com/wp-content/uploads/2019/09/Disney-Logo.jpeg',
-        characteristics: [
-          "Cuenta estable, tarifa prepaga de 12 meses",
-          "Clasificación de cine: 18+/R21/TV-MA",
-          "Entrega en tiempo real",
-        ],
-      },
-      {
-        id: 3,
-        name: 'Max',
-        price: 10000,
-        image: 'https://filmhounds.co.uk/wp-content/uploads/2023/05/max-logo.jpg',
-        characteristics: [
-          "Renovar la misma suscripción",
-          "Mantenga todos sus favoritos y listas",
-          "Entrega en tiempo real",
-        ],
-      },
-      {
-        id: 4,
-        name: 'Prime Video',
-        price: 10000,
-        image: 'https://th.bing.com/th/id/OIP.hOyeETmfMNoeV9ThyQMNBQHaHa?cb=iwc2&rs=1&pid=ImgDetMain',
-        characteristics: [],
-      },
-        {
-        id: 13,
-        name: 'vix premium',
-        price: 7000,
-        image: 'https://th.bing.com/th/id/OIP.fpsYx-rEZwqdPUUznf2I8QHaD4?cb=iwc2&rs=1&pid=ImgDetMain',
-        characteristics: [     ],
-      },
-      {
-        id: 6,
-        name: 'ChatGPT Plus',
-        price: 20000,
-        image: 'https://th.bing.com/th/id/OIP.PcgoreK_H_utzQX-eTNbrQHaHa?rs=1&pid=ImgDetMain',
-        characteristics: [
-          "voz de larga duracion",
-          "ia actualizada y deslimitada",
-          "Entrega en tiempo real",
-"cuenta compartida",
-        ],
-      },
-        {
-        id: 14,
-        name: 'Apple One',
-        price: 22000,
-        image: 'https://static.iphoneitalia.com/wp-content/uploads/2020/09/apple-one.png',
-        characteristics: [
-          "345 gb en icloud",
-          "con tu mismo apple id",
-          "apple tv, apple music, apple fitness y apple arcade.",
-        ],
-      },
-        {
-        id: 15,
-        name: 'MICROSOFT 365',
-        price: 12000,
-        image: 'https://th.bing.com/th/id/OIP.9vkItxSZEt6JEXNQy_sVcQHaEJ?cb=iwc2&rs=1&pid=ImgDetMain',
-        characteristics: [
-          "1 tb de almacenamiento",
-          "suite de office con word, excel, power point",
-          "con tu mismo correo de microsoft",
-        ],
-      },
-      {
+    {
+        id: 1,
+        name: 'Netflix',
+        price: 13000,
+        image: 'https://th.bing.com/th/id/R.64d5f8b96325d2be917f9ab402e92b99?rik=1BawFvPFPBGxrg&pid=ImgRaw&r=0',
+        characteristics: [
+            "Cuenta estable y renovable",
+            "Alta definición 4K + HDR con su lista exclusiva",
+            "Entrega en tiempo real",
+        ],
+    },
+    {
+        id: 2,
+        name: 'Netflix Extra',
+        price: 17000,
+        image: 'https://th.bing.com/th/id/R.64d5f8b96325d2be917f9ab402e92b99?rik=1BawFvPFPBGxrg&pid=ImgRaw&r=0',
+        characteristics: [
+            "Cuenta completa con un perfil",
+            "Alta definición 4K + HDR con su lista exclusiva",
+            "correo personalizado, ideal para usos en diferentes locaciones",
+        ],
+    },
+    {
+        id: 5,
+        name: 'Disney+',
+        price: 13000,
+        image: 'https://mdcthereporter.com/wp-content/uploads/2019/09/Disney-Logo.jpeg',
+        characteristics: [
+            "Cuenta estable, tarifa prepaga de 12 meses",
+            "Clasificación de cine: 18+/R21/TV-MA",
+            "Entrega en tiempo real",
+        ],
+    },
+    {
+        id: 3,
+        name: 'Max',
+        price: 10000,
+        image: 'https://filmhounds.co.uk/wp-content/uploads/2023/05/max-logo.jpg',
+        characteristics: [
+            "Renovar la misma suscripción",
+            "Mantenga todos sus favoritos y listas",
+            "Entrega en tiempo real",
+        ],
+    },
+    {
+        id: 4,
+        name: 'Prime Video',
+        price: 10000,
+        image: 'https://th.bing.com/th/id/OIP.hOyeETmfMNoeV9ThyQMNBQHaHa?cb=iwc2&rs=1&pid=ImgDetMain',
+        characteristics: [],
+    },
+    {
+        id: 13,
+        name: 'vix premium',
+        price: 7000,
+        image: 'https://th.bing.com/th/id/OIP.fpsYx-rEZwqdPUUznf2I8QHaD4?cb=iwc2&rs=1&pid=ImgDetMain',
+        characteristics: [],
+    },
+    {
+        id: 6,
+        name: 'ChatGPT Compartida',
+        price: 15000,
+        image: 'https://th.bing.com/th/id/OIP.PcgoreK_H_utzQX-eTNbrQHaHa?rs=1&pid=ImgDetMain',
+        characteristics: [
+            "Cuenta compartida",
+            "ia actualizada y deslimitada",
+            "Entrega en tiempo real",
+        ],
+    },
+    {
+        id: 21, // ID nuevo para la opción individual
+        name: 'ChatGPT Individual',
+        price: 35000,
+        image: 'https://th.bing.com/th/id/OIP.PcgoreK_H_utzQX-eTNbrQHaHa?rs=1&pid=ImgDetMain',
+        characteristics: [
+            "Cuenta personal y privada",
+            "Acceso completo a todas las funciones plus",
+            "Usa tu propio correo electrónico",
+            "Ideal para uso profesional y personal",
+        ],
+    },
+    {
+        id: 14,
+        name: 'Apple One',
+        price: 22000,
+        image: 'https://static.iphoneitalia.com/wp-content/uploads/2020/09/apple-one.png',
+        characteristics: [
+            "345 gb en icloud",
+            "con tu mismo apple id",
+            "apple tv, apple music, apple fitness y apple arcade.",
+        ],
+    },
+    {
+        id: 15,
+        name: 'MICROSOFT 365',
+        price: 12000,
+        image: 'https://th.bing.com/th/id/OIP.9vkItxSZEt6JEXNQy_sVcQHaEJ?cb=iwc2&rs=1&pid=ImgDetMain',
+        characteristics: [
+            "1 tb de almacenamiento",
+            "suite de office con word, excel, power point",
+            "con tu mismo correo de microsoft",
+        ],
+    },
+    {
         id: 20,
         name: 'MICROSOFT Compartida',
         price: 5000,
         image: 'https://th.bing.com/th/id/OIP.9vkItxSZEt6JEXNQy_sVcQHaEJ?cb=iwc2&rs=1&pid=ImgDetMain',
         characteristics: [
-          "Cuenta compartida con correo nuestro",
-          "Ideal si quieres una tener office original menor costo",
-          "almacenamiento de onedrive",
+            "Cuenta compartida con correo nuestro",
+            "Ideal si quieres una tener office original menor costo",
+            "almacenamiento de onedrive",
         ],
-      },
-      {
-        id: 17,
-        name: 'Coworking Alquiler',
-        price: 50000,
-        image: '/plataform/alquilar.png',
-        characteristics: [
-          "Espacio agradable y acojedor",
-          "café y cerveza gratis!",
-          "internet de alta velocidad",
-        ],
-      },
-
-      {
-        id: 8,
-        name: 'YouTube Premium',
-        price: 12000,
-        image: 'https://cdn.pixabay.com/photo/2021/09/11/18/21/youtube-6616310_1280.png',
-        characteristics: [
-          "Use su cuenta personal de YouTube para unirse a premium",
-          "Reproduzca videos en segundo plano",
-          "Disfrute de YouTube Music y YouTube Premium",
-        ],
-      },
-      {
-        id: 9,
-        name: 'Paramount+',
-        price: 6000,
-        image: 'https://th.bing.com/th/id/R.b80c8814890d78ad63b43c0bc2f4b401?rik=M95rQ%2fqeKRSHPQ&pid=ImgRaw&r=0',
-        characteristics: [
-          "Calidad de video HDR 4K",
-          "Audio premium sin publicidad",
-          "Entrega en tiempo real",
-        ],
-      },
-      {
-        id: 10,
-        name: 'Spotify',
-        price: 9000,
-        image: 'https://upload.wikimedia.org/wikipedia/commons/7/74/Spotify_App_Logo.svg',
-        characteristics: [
-          "Con tu misma cuenta que manejabas antes",
-          "No estará molestado por otros",
-          "Cuenta estable",
-        ],
-      },
-      {
-        id: 11,
-        name: 'Crunchyroll',
-        price: 7000,
-        image: 'https://i.pinimg.com/originals/d3/af/7d/d3af7d3ac568b0890b42370d2be192c8.png',
-        characteristics: [
-          "Cuenta estable, tarifa prepaga de 12 meses",
-          "Clasificación de cine: 18+/R21/TV-MA",
-          "Todo el anime disponible",
-        ],
-      },
-      {
-        id: 12,
-        name: 'Xbox Game Pass',
-        price: 30000,
-        image: 'https://th.bing.com/th/id/R.6b6125b82e9694dae2125c36b7d917c3?rik=aFR3QZK9HRmLIw&pid=ImgRaw&r=0',
-        characteristics: [
-          "Renovar la misma suscripción",
-          "Juegue en la nube con VPN",
-          "Acceso a más de 1000 juegos",
-        ],
-      },
-  {
-      id: 16, // Assuming the next available ID
-      name: 'Suscripción Platzi Expert',
-      price: 60000,
-      image: 'https://latamlist.com/wp-content/uploads/2018/05/platzi-logo-huge.png', // Or another appropriate Platzi logo/image URL
-      characteristics: [
-        "Acceso a todo el catálogo de más de 1200 cursos",
-        "Rutas de aprendizaje especializadas",
-        "Certificados profesionales al finalizar los cursos",
-        "Acceso a la comunidad y eventos exclusivos",
-      ],
     },
-      {
-    id: 18, // New ID
-    name: 'IPTV',
-    price: 10000, // Example price
-    image: 'https://play-lh.googleusercontent.com/TtmQHrL1HRwBzyMSkT5XOqZZcGDqjy9265IqSEKpgKxJDgSkZhXRHuRa-eSwWzPZqW0=w480-h960-rw', // Generic IPTV image
-    characteristics: [
-      "Más de 10,000 canales en vivo nacionales e internacionales incluidos WIN + Y DIRECTV",
-      "Catálogo de películas y series actualizado (VOD)",
-      "Calidad de video HD, FHD y 4K",
-      "Compatible con Smart TV, TV Box, celulares y computadores",
-    ],
-  },
-  {
-      id: 19,
-      name: 'Gemini Pro',
-      price: 20000,
-      image: 'https://meta-q.cdn.bubble.io/f1724422026295x667563819270324800/GeminiStreamingPro2.svg',
-      characteristics: [
-        "Acceso a nuestro modelo más potente 2.5 Pro",
-        "Razonamiento lógico y seguimiento de instrucciones con detalles sutiles",
-        "Capacidad para analizar y resumir materiales extensos (hasta 1,500 páginas)",
-        "Integrado en Gmail, Documentos, Presentaciones y más",
-        "Funciones de codificación avanzadas",
-        "2 TB de almacenamiento en Google One",
-      ],
+    {
+        id: 17,
+        name: 'Coworking Alquiler',
+        price: 50000,
+        image: '/plataform/alquilar.png',
+        characteristics: [
+            "Espacio agradable y acojedor",
+            "café y cerveza gratis!",
+            "internet de alta velocidad",
+        ],
     },
-    ];
+    {
+        id: 8,
+        name: 'YouTube Premium',
+        price: 12000,
+        image: 'https://cdn.pixabay.com/photo/2021/09/11/18/21/youtube-6616310_1280.png',
+        characteristics: [
+            "Use su cuenta personal de YouTube para unirse a premium",
+            "Reproduzca videos en segundo plano",
+            "Disfrute de YouTube Music y YouTube Premium",
+        ],
+    },
+    {
+        id: 9,
+        name: 'Paramount+',
+        price: 6000,
+        image: 'https://th.bing.com/th/id/R.b80c8814890d78ad63b43c0bc2f4b401?rik=M95rQ%2fqeKRSHPQ&pid=ImgRaw&r=0',
+        characteristics: [
+            "Calidad de video HDR 4K",
+            "Audio premium sin publicidad",
+            "Entrega en tiempo real",
+        ],
+    },
+    {
+        id: 10,
+        name: 'Spotify',
+        price: 9000,
+        image: 'https://upload.wikimedia.org/wikipedia/commons/7/74/Spotify_App_Logo.svg',
+        characteristics: [
+            "Con tu misma cuenta que manejabas antes",
+            "No estará molestado por otros",
+            "Cuenta estable",
+        ],
+    },
+    {
+        id: 11,
+        name: 'Crunchyroll',
+        price: 7000,
+        image: 'https://i.pinimg.com/originals/d3/af/7d/d3af7d3ac568b0890b42370d2be192c8.png',
+        characteristics: [
+            "Cuenta estable, tarifa prepaga de 12 meses",
+            "Clasificación de cine: 18+/R21/TV-MA",
+            "Todo el anime disponible",
+        ],
+    },
+    {
+        id: 12,
+        name: 'Xbox Game Pass',
+        price: 30000,
+        image: 'https://th.bing.com/th/id/R.6b6125b82e9694dae2125c36b7d917c3?rik=aFR3QZK9HRmLIw&pid=ImgRaw&r=0',
+        characteristics: [
+            "Renovar la misma suscripción",
+            "Juegue en la nube con VPN",
+            "Acceso a más de 1000 juegos",
+        ],
+    },
+    {
+        id: 16,
+        name: 'Suscripción Platzi Expert',
+        price: 60000,
+        image: 'https://latamlist.com/wp-content/uploads/2018/05/platzi-logo-huge.png',
+        characteristics: [
+            "Acceso a todo el catálogo de más de 1200 cursos",
+            "Rutas de aprendizaje especializadas",
+            "Certificados profesionales al finalizar los cursos",
+            "Acceso a la comunidad y eventos exclusivos",
+        ],
+    },
+    {
+        id: 18,
+        name: 'IPTV',
+        price: 10000,
+        image: 'https://play-lh.googleusercontent.com/TtmQHrL1HRwBzyMSkT5XOqZZcGDqjy9265IqSEKpgKxJDgSkZhXRHuRa-eSwWzPZqW0=w480-h960-rw',
+        characteristics: [
+            "Más de 10,000 canales en vivo nacionales e internacionales incluidos WIN + Y DIRECTV",
+            "Catálogo de películas y series actualizado (VOD)",
+            "Calidad de video HD, FHD y 4K",
+            "Compatible con Smart TV, TV Box, celulares y computadores",
+        ],
+    },
+    {
+        id: 19,
+        name: 'Gemini Pro',
+        price: 20000,
+        image: 'https://meta-q.cdn.bubble.io/f1724422026295x667563819270324800/GeminiStreamingPro2.svg',
+        characteristics: [
+            "Acceso a nuestro modelo más potente 2.5 Pro",
+            "Razonamiento lógico y seguimiento de instrucciones con detalles sutiles",
+            "Capacidad para analizar y resumir materiales extensos (hasta 1,500 páginas)",
+            "Integrado en Gmail, Documentos, Presentaciones y más",
+            "Funciones de codificación avanzadas",
+            "2 TB de almacenamiento en Google One",
+        ],
+    },
+];
